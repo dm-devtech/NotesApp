2a61cf5adb23d6384f243061504f3472673e9deb
David Millen <70208036+dm-devtech@users.noreply.github.com>
2020-11-11 14:38:27 +0000

    Added tests to module pattern

--- test/note-list-tests.js
+++ test/note-list-tests.js
@@ -1,26 +1,23 @@
-function testNotesArrayExists() {
+(function testNotesArrayExists() {
   var note = new Notelist();
   assert.isTrue(Array.isArray(note.savednotes));
-};
+})();
 
-function testNotesArrayLength() {
+(function testNotesArrayLength() {
   var note = new Notelist();
   assert.isTrue(Array.length === 1);
-};
+})();
 
-function testOutputNotes() {
+(function testOutputNotes() {
   var notelist = new Notelist();
   notelist.addNote("test")
   assert.isTrue(notelist.savednotes[0].savednote === "test");
-};
+})();
 
-function testReturnsAllNotes() {
+(function testReturnsAllNotes() {
   var notelist = new Notelist();
   notelist.addNote("test")
   assert.isTrue(notelist.returnNotes() === "test");
-};
+})();
 
-testNotesExist();
-testNotesArrayLength();
-testOutputNotes();
-testReturnsAllNotes()
+testNotesExist()
--- test/note-tests.js
+++ test/note-tests.js
@@ -1,14 +1,18 @@
 // note-tests.js
 
-function testNotesExist() {
+(function testNotesExist(exports) {
   var note = new Note("My favourite language is JavaScript");
-  assert.isTrue(note.savednote === "My favourite language is JavaScript");
-};
 
-function testNotesReturn() {
+    function test() { console.log("prints");};
+  assert.isTrue(note.savednote === "My favourite language is JavaScript");
+
+  exports.test = test
+})(this);
+
+(function testNotesReturn() {
   var note = new Note("My favourite language is JavaScript");
   assert.isTrue(note.returnNote() === "My favourite language is JavaScript");
-};
+})();
 
-testNotesExist();
-testNotesReturn()
+// testNotesExist();
+// testNotesReturn()
